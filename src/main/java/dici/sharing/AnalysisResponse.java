10e7fc230c3a66f7398a14aa83a1b38420309a7a
Maxime <129526478+Maximeuuu@users.noreply.github.com>
2025-01-22 19:21:07 +0100

    Update AnalysisResponse.java
    ajout du code dans les méthodes

--- src/main/java/dici/sharing/AnalysisResponse.java
+++ src/main/java/dici/sharing/AnalysisResponse.java
@@ -5,26 +5,35 @@ import javax.swing.JPanel;
 import dici.stats.methods.IMethod;
 
 public class AnalysisResponse {
-	private IMethod statMethod;
+	//private IMethod statMethod;
 
-	/*private JPanel panelGraph;
+	private JPanel panelGraph;
 	private boolean isCorrelated;
 	private String correlationStrength;
-	private double correlationValue;*/
+	private double correlationValue;
 
 	public AnalysisResponse( IMethod statMethod ){
-		this.statMethod = statMethod;
+		this.correlationValue = statMethod.compute();
+  this.isCorrelated = PearsonTools.isCorrelated( this.correlationValue );
+  this.correlationStrength = PearsonTools.getStringCorrelation( this.correlationValue );
+
+  LineGraph lineGraph = new LineGraph( statsMethod.getXYCollection() ); 
+  this.panelGraph = lineGraph.getPanel();
 	}
 
 	public JPanel getPanelGraph( ){
+  return this.panelGraph;
 	}
 
 	public boolean isCorrelated( ){
+  return this.isCorrelated;
 	}
 
 	public String getCorrelationStrength( ){
+  return this.correlationStrength;
 	}
 
 	public double getCorrelationValue( ){
+  return this.correlationValue;
 	}
 }
