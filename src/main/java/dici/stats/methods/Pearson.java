4b3b737242fa10be84c2f36827c9e564c8ba7f2e
Maxime <129526478+Maximeuuu@users.noreply.github.com>
2025-01-23 02:37:40 +0100

    Update Pearson.java
    maj calcul

--- src/main/java/dici/stats/methods/Pearson.java
+++ src/main/java/dici/stats/methods/Pearson.java
@@ -3,6 +3,56 @@ package dici.stats.methods;
 import dici.stats.objects.XYCollection;
 
 public class Pearson implements IMethod {
+    private XYCollection datas;
+
+    public Pearson(XYCollection datas) {
+        this.datas = datas;
+    }
+
+    @Override
+    public XYCollection getXYCollection() {
+        return this.datas;
+    }
+
+    @Override
+    public double compute() {
+        // Calcul du numérateur
+        double numerator = 0;
+        double avgX = this.datas.getValuesX().avg();
+        double avgY = this.datas.getValuesY().avg();
+        
+        // On calcule la somme des produits (xi - moyenneX) * (yi - moyenneY)
+        for (int i = 0; i < this.datas.size(); i++) {
+            double x = this.datas.getValuesX().getValue(i);
+            double y = this.datas.getValuesY().getValue(i);
+            numerator += (x - avgX) * (y - avgY);
+        }
+
+        // Calcul du dénominateur (écarts-types des deux variables)
+        double denominatorX = 0;
+        double denominatorY = 0;
+
+        for (int i = 0; i < this.datas.size(); i++) {
+            double x = this.datas.getValuesX().getValue(i);
+            double y = this.datas.getValuesY().getValue(i);
+            denominatorX += Math.pow(x - avgX, 2);
+            denominatorY += Math.pow(y - avgY, 2);
+        }
+
+        // Racine carrée des sommes des carrés
+        double denominator = Math.sqrt(denominatorX * denominatorY);
+
+        // Retourne le coefficient de corrélation de Pearson
+        return numerator / denominator;
+    }
+}
+
+// je pense qu'elle n'est pas bonne
+/*package dici.stats.methods;
+
+import dici.stats.objects.XYCollection;
+
+public class Pearson implements IMethod {
 	private XYCollection datas;
 
 	public Pearson( XYCollection datas ){
@@ -39,4 +89,4 @@ public class Pearson implements IMethod {
 
 		return Math.sqrt(sumX * sumY);
 	}
-}
+}*/
